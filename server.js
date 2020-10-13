f2e578319ca888b10e2d97b5788c3b8b46074d91
MrFaly <faliana.razafitsoma@gmail.com>
2020-10-13 14:46:59 +0200

    node module deletion

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
+    res.redirect(`/${uuidv4()}`)
 })
 
 app.get('/:party', (req, res) => {
     res.render('party', { partyId: req.params.party })
+})
+
+io.on('connection', socket => {
+    socket.on('join-party', (partyId, userId) => {
+        console.log(partyId, userId)
+    })
 })
 
 server.listen(3000)
