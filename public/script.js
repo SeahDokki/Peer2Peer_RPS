e1ef077e21491e679333fc2108ff22e839896333
pierre.deroubaix935@gmail.com <aswzdxefc751>
2020-10-20 11:16:27 +0200

    hotfix

--- public/script.js
+++ public/script.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
         let partyId = $('#partyId_input').val();
         let userId = user.id;
         socket.emit('join-party',partyId ,userId);
+        window.location.href = "game/"+partyId;
     })
 
     function createUser(username)
